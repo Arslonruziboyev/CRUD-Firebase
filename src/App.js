013d4aa3e19ada54e6a1c339fb32189d3cf4162b
02arslon <arslonruziboyev@gmail.com>
2023-05-13 18:40:28 +0900

    changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [isSinup, setIsSignup] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="text-center">
+        <h1>{isSinup ? "Siginup" : "Login"}</h1>
+        <br />
+        <form>
+          {isSinup && (
+            <div>
+              <label htmlFor="username">Username</label>
+              <br />
+              <input type="text" id="username" />
+            </div>
+          )}
+          <div>
+            <label htmlFor="email">Email</label>
+            <br />
+            <input type="text" name="" id="email" />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <br />
+            <input type="password" name="" id="password" />
+          </div>
+          {isSinup ? <button>Siginup</button> : <button>Login</button>}
+        </form>
+        <button type="button">
+          {isSinup ? "Alredy have an account?Login" : "Dont Have An Account"}
+        </button>
+      </div>
     </div>
   );
 }
